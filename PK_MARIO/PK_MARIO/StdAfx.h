c3bb75a637dcad3152daecd4340f32d778c1c8f5
Kacper Wójcicki <kacpwoj585@student.polsl.pl>
2016-06-12 15:44:04 +0200

    Startup

---
+++ PK_MARIO/PK_MARIO/StdAfx.h
@@ -0,0 +1,63 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+//#pragma once
+
+//#include "targetver.h"
+
+//#include <stdio.h>
+//#include <tchar.h>
+
+
+
+// TODO: reference additional headers your program requires here
+
+#define USE_PRECOMPILED_HEADERS
+
+#ifndef __STDAFX_H_INCLUDED__
+#define __STDAFX_H_INCLUDED__
+
+#ifdef USE_PRECOMPILED_HEADERS
+
+
+//#include "targetver.h"
+#ifndef _WIN32_WINNT            // Specifies that the minimum required platform is Windows Vista.
+#define _WIN32_WINNT 0x0600     // Change this to the appropriate value to target other versions of Windows.
+#endif
+// end of targetver.h
+#define NOMINMAX
+#include <sstream>
+#include <iostream>
+#include <fstream>
+
+#include <stdexcept>  // logic_error
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+#include <cctype>
+
+
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_mixer.h>
+#include <SDL_image.h>
+
+#undef main
+#include <shared_ptr.hpp>
+#include <enable_shared_from_this.hpp>
+
+#include <string>
+#include <vector> 
+#include <list>
+#include <map>
+#include <algorithm>
+#include <utility>
+
+//#include <cstdio>
+//#include <tchar.h>
+
+#endif  // USE_PRECOMPILED_HEADERS
+#endif  // __STDAFX_H_INCLUDED__
---
+++ PK_MARIO/PK_MARIO/main.cpp
@@ -0,0 +1,13 @@
+#include "StdAfx.h"
+
+
+
+int main(/* int argc, char *argv[] */) {
+	std::cout << "      strza³ki lewo/prawo  -  poruszanie siê postaci¹\n"
+		<< "         strza³ka do góry  -  skok\n"
+		<< " przytrzymanie klawisza d  -  bieganie\n"
+		<< "   naciœniêcie klawisza s  -  strzelanie\n"
+		<< std::endl;
+
+	return 0;
+}
